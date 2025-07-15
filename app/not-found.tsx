0aceb8ca7926ea9e8b49b45a3cea5b8d209f180a
Svitlana <missis.chuvaeva@ukr.net>
2025-07-15 23:34:07 +0300

    not-found-page changes

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,51 +1,34 @@
-'use client';
-
-import Link from 'next/link';
-import { useEffect } from 'react';
-import { useRouter } from 'next/navigation';
+import { Metadata } from 'next';
 import css from './not-found.module.css';
-import type { Metadata } from "next";
-
-
 
 export const metadata: Metadata = {
-  title: 'Сторінку не знайдено — NoteHub',
-  description: 'На жаль, сторінка, яку ви шукаєте, не існує.',
+  title: 'Page not found',
+  description:
+    "The page you're looking for doesn't exist or has been moved. Try going back to the homepage or explore other notes.",
   openGraph: {
-    title: 'Сторінку не знайдено — NoteHub',
-    description: 'Ця сторінка не існує. Спробуйте іншу адресу.',
-    url: 'https://08-zustand-cyan.vercel.app',
+    title: 'Page not found',
+    description:
+      "The page you're looking for doesn't exist or has been moved. Try going back to the homepage or explore other notes.",
     images: [
       {
-      url: 'https://ac.goit.global/fullstack/react/notehub-og-meta.jpg',
-      width: 1200,
-      height: 630,
-      
-      }
-
-      ],
+        url: '/page-note-found.jpeg',
+        width: 1200,
+        height: 630,
+        alt: 'page not found',
+      },
+    ],
+    siteName: 'NoteHub',
+    url: 'https://08-zustand-zeta.vercel.app/not-found',
   },
 };
 
-const NotFound = () => {
-  const router = useRouter();
-
-  useEffect(() => {
-    const timer = setTimeout(() => router.push('/'), 3000);
-    return () => clearTimeout(timer);
-  }, [router]);
-
+export default function NotFound() {
   return (
-    <main className={css.container}>
+    <>
       <h1 className={css.title}>404 - Page not found</h1>
       <p className={css.description}>
         Sorry, the page you are looking for does not exist.
       </p>
-      <Link href="/" className={css.homeLink}>
-        Go back home
-      </Link>
-    </main>
+    </>
   );
-};
-
-export default NotFound;
+}
